5e80c9c72edd96aafe49daa74673806603491de2
joaomarcos08 <49624480+joaomarcos08@users.noreply.github.com>
2020-04-30 18:03:32 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+module.exports = [
+    { 
+        id:"sBA_SDhIPqQ",
+        title:"Utilizando React.memo",
+        duration:"19mins",
+        price:"Free",
+        featured: true,
+    },
+
+    { 
+        id:"p6d3RzhgQbA",
+        title:"Criando sua própria template no React Native",
+        duration:"7mins",
+        price:"Free",
+    },
+
+    { 
+        id:"mcd7lqpUzIA",
+        title:"Criando conribuição open-source no Github do zero!",
+        duration:"42mins",
+        price:"Free"
+    },
+
+    { 
+        id:"Rt-xG_VzD6M",
+        title:"Criando uma CLI com NodeJS e Gluegun",
+        duration:"36mins",
+        price:"Free",
+        featured: true
+    },
+
+    { 
+        id:"y5Hv7pMA1uo",
+        title:"React Native + RealmnDB",
+        duration:"57mins",
+        price:"Free"
+    },
+
+    { 
+        id:"-j7vLmBMsEU",
+        title:"Deplou de aplicação NodeJs e ReactJs no Heroku",
+        duration:"24mins",
+        price:"Free",
+        featured: true
+    },
+]
